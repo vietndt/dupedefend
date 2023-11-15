d8b0166adcf654a28f7db77f8d423fa2936cd677
Viet <nguyendoantuanviet@gmail.com>
2023-11-15 23:17:05 +0700

    Update frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,18 +1,49 @@
 import { Container } from "@mui/material";
 
+import '@rainbow-me/rainbowkit/styles.css';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
 import Associate from "./components/Associate";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { polygonMumbai } from "viem/chains";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'DupeDefend App',
+  projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID as string,
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+});
 
 function App() {
   return (
-    <Container sx={{
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      minHeight: '100vh'
-    }} disableGutters maxWidth={false}>
-      <Associate />
-    </Container>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Container sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          minHeight: '100vh'
+        }} disableGutters maxWidth={false}>
+          <Associate />
+        </Container>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- packages/frontend/src/components/Associate.tsx
+++ packages/frontend/src/components/Associate.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import { Box, FormControl, TextField, Button, Paper, Typography } from "@mui/material";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Associate = () => {
+  const [videoInputControl, setVideoInputControl] = useState<string>('');
+  const [snippet, setSnippet] = useState<any>();
+
+  const getVideoDetail = (id: string) => {
+    fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`,)
+      .then(response => {
+        if (response.ok) return response.json();
+      }).then(json => {
+        if (json.items && json.items[0] && json.items[0].snippet) {
+          setSnippet(json.items[0].snippet)
+        }
+      });
+  }
+
   return (
     <Box component={Paper} sx={{
       display: 'flex',
@@ -10,6 +26,7 @@ const Associate = () => {
       padding: 3,
       width: '100%'
     }}>
+      <ConnectButton />
       <Typography component="h3" sx={{
         fontSize: 20,
         fontWeight: 600
@@ -24,21 +41,36 @@ const Associate = () => {
           width: { xs: '100%' }
         }}>
           <TextField
-            label="Ethereum Address *"
-            helperText=""
-          />
-        </FormControl>
-        <FormControl sx={{
-          width: { xs: '100%' }
-        }}>
-          <TextField
-            label="Video URL *"
-            helperText=""
+            label="Video ID or Video URL *"
+            helperText="E.g. jNQXAC9IVRw  or  https://www.youtube.com/watch?v=jNQXAC9IVRw"
+            value={videoInputControl}
+            onChange={(event) => {
+              const value = event.target.value;
+              setVideoInputControl(value);
+            }}
           />
         </FormControl>
         <Box>
-          <Button variant="contained">Associate</Button>
+          <Button variant="contained" disabled={!videoInputControl} onClick={() => {
+            let id = '';
+            if (videoInputControl.indexOf('?v=') !== -1) {
+              id = videoInputControl.slice(videoInputControl.indexOf('?v=') + 3, videoInputControl.length);
+            } else {
+              id = videoInputControl;
+            }
+            getVideoDetail(id);
+          }}>Associate</Button>
         </Box>
+        {snippet ?
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%'
+          }}>
+            <Typography>ChannelId: {snippet.channelId}</Typography>
+            <Typography>Title: {snippet.title}</Typography>
+            <Typography>Description: {snippet.description}</Typography>
+          </Box> : <></>}
       </Box>
     </Box>
   )
